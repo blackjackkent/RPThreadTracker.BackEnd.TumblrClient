0dc2dedbd429297a2aaddfd62ad80779efbd54f2
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2018-10-06 21:48:33 -0500

    Fixing logging levels

--- RPThreadTrackerV3.BackEnd.TumblrClient/Program.cs
+++ RPThreadTrackerV3.BackEnd.TumblrClient/Program.cs
@@ -5,67 +5,67 @@
 
 namespace RPThreadTrackerV3.BackEnd.TumblrClient
 {
-    using System;
-    using System.Diagnostics.CodeAnalysis;
-    using Microsoft.AspNetCore;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.Logging;
-    using NLog.Web;
+	using System;
+	using System.Diagnostics.CodeAnalysis;
+	using Microsoft.AspNetCore;
+	using Microsoft.AspNetCore.Hosting;
+	using Microsoft.Extensions.Configuration;
+	using Microsoft.Extensions.Logging;
+	using NLog.Web;
 
-    /// <summary>
-    /// Base application bootstrapping file.
-    /// </summary>
-    [ExcludeFromCodeCoverage]
-    public class Program
-    {
-        /// <summary>
-        /// Defines the entry point of the application.
-        /// </summary>
-        /// <param name="args">The application arguments.</param>
-        public static void Main(string[] args)
-        {
-            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
-            try
-            {
-                logger.Debug("Initializing RPThreadTrackerV3.BackEnd.TumblrClient");
-                BuildWebHost(args).Run();
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex, "Stopped RPThreadTrackerV3.BackEnd.TumblrClient");
-                throw;
-            }
-            finally
-            {
-                NLog.LogManager.Shutdown();
-            }
-        }
+	/// <summary>
+	/// Base application bootstrapping file.
+	/// </summary>
+	[ExcludeFromCodeCoverage]
+	public class Program
+	{
+		/// <summary>
+		/// Defines the entry point of the application.
+		/// </summary>
+		/// <param name="args">The application arguments.</param>
+		public static void Main(string[] args)
+		{
+			var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+			try
+			{
+				logger.Debug("Initializing RPThreadTrackerV3.BackEnd.TumblrClient");
+				BuildWebHost(args).Run();
+			}
+			catch (Exception ex)
+			{
+				logger.Error(ex, "Stopped RPThreadTrackerV3.BackEnd.TumblrClient");
+				throw;
+			}
+			finally
+			{
+				NLog.LogManager.Shutdown();
+			}
+		}
 
-        /// <summary>
-        /// Builds the web host.
-        /// </summary>
-        /// <param name="args">The application arguments.</param>
-        /// <returns>Web host instance.</returns>
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .ConfigureAppConfiguration((builderContext, config) =>
-                {
-                    var env = builderContext.HostingEnvironment;
-                    config.Sources.Clear();
-                    config
-                        .AddJsonFile("appsettings.json", false, true)
-                        .AddJsonFile("appsettings.secure.json", true, true)
-                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true)
-                        .AddEnvironmentVariables();
-                })
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.SetMinimumLevel(LogLevel.Trace);
-                })
-                .UseNLog()
-                .Build();
-    }
+		/// <summary>
+		/// Builds the web host.
+		/// </summary>
+		/// <param name="args">The application arguments.</param>
+		/// <returns>Web host instance.</returns>
+		public static IWebHost BuildWebHost(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.ConfigureAppConfiguration((builderContext, config) =>
+				{
+					var env = builderContext.HostingEnvironment;
+					config.Sources.Clear();
+					config
+						.AddJsonFile("appsettings.json", false, true)
+						.AddJsonFile("appsettings.secure.json", true, true)
+						.AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true)
+						.AddEnvironmentVariables();
+				})
+				.ConfigureLogging(logging =>
+				{
+					logging.ClearProviders();
+					logging.SetMinimumLevel(LogLevel.Debug);
+				})
+				.UseNLog()
+				.Build();
+	}
 }
